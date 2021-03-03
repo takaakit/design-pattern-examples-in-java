17b9a932fb32353d4bbee0343a04be34e7cabc70
takaakit <takaaki.teshima.gh@gmail.com>
2021-03-03 20:38:03 +0900

    Edited comment

--- src/main/java/behavioralpatterns/chainofresponsibility/Main.java
+++ src/main/java/behavioralpatterns/chainofresponsibility/Main.java
@@ -2,10 +2,10 @@ package behavioralpatterns.chainofresponsibility;
 
 /*
 A trouble is turned around among supporters, and the trouble will be handled by the supporter who can handle it. There are four types of supporters below:
-* "LazySupporter" doesn't handle any trouble
-* "MoodySupporter" handles odd ID troubles
-* "SpecialSupporter" handles a trouble of the target ID.
-* "LimitedSupporter" handles troubles below the limit ID.
+* LazySupporter doesn't handle any trouble
+* MoodySupporter handles odd ID troubles
+* SpecialSupporter handles a trouble of the target ID.
+* LimitedSupporter handles troubles below the limit ID.
  */
 
 public class Main {
--- src/main/java/structuralpatterns/flyweight/Main.java
+++ src/main/java/structuralpatterns/flyweight/Main.java
@@ -1,7 +1,39 @@
 package structuralpatterns.flyweight;
 
 /*
-Display a string consisting of large characters. Large character objects are not created until they are needed. And the created objects are reused.
+Display a string consisting of large characters (numbers and hyphen only). Large character objects are not created until they are needed. And the created objects are reused.
+
+Example Output
+-----
+Please enter digits (ex. 1212123): 123
+              
+     ####     
+      ###     
+      ###     
+      ###     
+      ###     
+      ###     
+    #######   
+              
+
+              
+   ########   
+         ###  
+         ###  
+   ########   
+  #           
+  #           
+  ##########  
+              
+
+              
+   ########   
+         ###  
+         ###  
+   ########   
+         ###  
+  #      ###  
+   ########
  */
 
 import java.util.Scanner;
