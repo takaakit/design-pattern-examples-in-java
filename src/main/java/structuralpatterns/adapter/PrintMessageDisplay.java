eaba26e7f68ece70883d7e032f7464f85c206fd4
takaakit <takaaki.teshima.dev@gmail.com>
2022-07-25 18:20:25 +0900

    Add comments

--- src/main/java/structuralpatterns/adapter/PrintMessageDisplay.java
+++ src/main/java/structuralpatterns/adapter/PrintMessageDisplay.java
@@ -3,6 +3,7 @@ package structuralpatterns.adapter;
 
 // ˄
 
+// Adapt the MessageDisplay interface to the Print interface.
 public class PrintMessageDisplay extends MessageDisplay implements Print {
     // ˅
     
